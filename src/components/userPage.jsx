8d505557c46ab274a8a15a472cbb053ddd1f0619
manjeetsingh100001 <manjeetsingh100001@yahoo.com>
2022-05-31 15:44:28 +0530

    remove warning

--- src/components/userPage.jsx
+++ src/components/userPage.jsx
@@ -1,34 +1,35 @@
-import axios from "axios"
-import { useEffect,useState} from "react"
-import {Link} from "react-router-dom"
-function UserPage(){
-const [loading,setLoading]=useEffect(true);
-const [data,setData]=useEffect(null);
-useEffect(()=>{
-    setLoading(true);
+
+// import axios from "axios"
+// import { useEffect,useState} from "react"
+// import {Link} from "react-router-dom"
+// function UserPage(){
+// const [loading,setLoading]=useEffect(true);
+// const [data,setData]=useEffect(null);
+// useEffect(()=>{
+//     setLoading(true);
     
-    axios({
-        url:"https://reqres.in/api/users",
-        method:"GET"
+//     axios({
+//         url:"https://reqres.in/api/users",
+//         method:"GET"
     
-    })
-    .then((res)=>{
-        setLoading(false);
-        setData(res.data)
-    })
-    .catch((err)=>{
-        setLoading(false);
-    });
+//     })
+//     .then((res)=>{
+//         setLoading(false);
+//         setData(res.data)
+//     })
+//     .catch((err)=>{
+//         setLoading(false);
+//     });
 
 
-},[])
+// },[])
 
-console.log(data)
-    return(
-        <>
-<h1> All Users</h1>
+// console.log(data)
+//     return(
+//         <>
+// <h1> All Users</h1>
         
         
-        </>
-    )
-}export default UserPage;
+//         </>
+//     )
+// }export default UserPage;
--- src/routes/allRoutes.jsx
+++ src/routes/allRoutes.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "../components/home";
 import About from "../components/about";
-import UserPage from "../components/userPage";
+// import UserPage from "../components/userPage";
 
 
 function Allroutes(){
@@ -10,7 +10,7 @@ function Allroutes(){
         <Routes>
          <Route path="/" element={<Home/>}/>
          <Route path="/about" element={<About/>}/>
-         <Route path="/users" element={<UserPage/>}/>
+         {/* <Route path="/users" element={<UserPage/>}/> */}
 
          
 
